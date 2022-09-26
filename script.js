ff1ec6db165227b60ff3b26bb7f7f4a934da0595
Abishek <abishekhttps@gmail.com>
2022-09-26 14:49:43 +0530

    initial

---
+++ script.js
@@ -0,0 +1,40 @@
+let grid = 4
+
+clickedTiles = []
+let section = document.querySelector('section')
+
+
+for(row = 0; row<grid; row++)
+{
+    let wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
+
+  for(col = 0; col < grid; col++){
+    let tile = document.createElement('div')
+    tile.classList.add('tile')
+    wrapper.appendChild(tile)
+  }
+    section.appendChild(wrapper)
+}
+
+document.addEventListener('click', pattern)
+
+function pattern(e){
+    if(!e.target.classList.contains('tile')) return
+    if(clickedTiles.includes(e.target)) return
+    e.target.classList.add('selected')
+    clickedTiles.push(e.target)
+
+    let numberofTiles = document.querySelectorAll('.tile').length
+    
+
+    if(clickedTiles.length >= numberofTiles){
+        const interval = setInterval(() =>{
+            const tile = clickedTiles.shift()
+            tile.classList.remove('selected')
+            if(clickedTiles.length === 0){
+                clearInterval(interval)
+            }
+        },1000)
+    }
+}
